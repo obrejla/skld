f8ab78e83ca11522ec1c2175610cc80a4b8d44e8
Ondrej Brejla <ondrej@brejla.cz>
2017-10-27 09:35:11 +0200

    Added actions to handle Customers init flags.

--- src/reducers/customers.js
+++ src/reducers/customers.js
@@ -4,6 +4,26 @@ import {
     UPDATE_CUSTOMERS,
 } from '../actions/index';
 
+const initCustomers = (state = false, action) => {
+    switch (action.type) {
+        case INIT_CUSTOMERS:
+            return true;
+        case UPDATE_CUSTOMERS:
+            return false;
+        default:
+            return state;
+    }
+};
+
+const customersDidInit = (state = false, action) => {
+    switch (action.type) {
+        case INIT_CUSTOMERS:
+            return true;
+        default:
+            return state;
+    }
+};
+
 const customers = (state = [], action) => {
     switch (action.type) {
         case UPDATE_CUSTOMERS:
@@ -14,8 +34,12 @@ const customers = (state = [], action) => {
 };
 
 export default combineReducers({
+    initCustomers,
+    customersDidInit,
     customers,
 });
 
 export const getAll = state => state.customers;
 export const getCustomer = (state, customerId) => state.customers.find(customer => customer.id === customerId);
+export const isCustomersDidInit = state => state.customersDidInit;
+export const isInitCustomers = state => state.initCustomers;
